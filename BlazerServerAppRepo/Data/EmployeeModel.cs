2f9ea83bc69e5e3cfa6226397f3fe2e061b8be0d
varun <varunlad5@gmail.com>
2021-12-05 17:13:24 +0530

    [Varun]Add:Model class is Added

---
+++ BlazerServerAppRepo/Data/EmployeeModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazerServerAppRepo.Data
+{
+    public class EmployeeModel
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string ProfileImage { get; set; }
+        public string Gender { get; set; }
+        public string Department { get; set; }
+        public int Salary { get; set; }
+        public DateTime StartDate { get; set; }
+        public string Notes { get; set; }
+    }
+}
