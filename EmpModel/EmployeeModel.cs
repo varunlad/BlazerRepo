6cd2dd3f9c0b8c3482568c983936d3cb5f5b9b1f
varun <varunlad5@gmail.com>
2021-12-08 18:28:51 +0530

    [Varun]Add:Separate Model Layer is Created

--- EmpModel/EmployeeModel.cs
+++ EmpModel/EmployeeModel.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
 
-namespace BlazerServerAppRepo.Data
+namespace EmpModel
 {
-    public class EmployeeModel
+   public class EmployeeModel
     {
         public int EmployeeId { get; set; }
         public string Name { get; set; }
