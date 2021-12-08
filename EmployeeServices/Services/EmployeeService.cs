26f069d94bda1ac09d038f8500d7d63ae998c3b7
varun <varunlad5@gmail.com>
2021-12-08 18:36:13 +0530

    [Varun]Add:Separate Services Layer is Created

---
+++ EmployeeServices/Services/EmployeeService.cs
@@ -0,0 +1,43 @@
+﻿using EmpDataAccess;
+using EmpModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EmployeeServices
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly IEmployeeDataAccess employeeDataAccess;
+        public EmployeeService(IEmployeeDataAccess employeeDataAccess)
+        {
+            this.employeeDataAccess = employeeDataAccess;
+        }
+        public List<EmployeeModel> GetEmploess()
+        {
+            List<EmployeeModel> emps = employeeDataAccess.GetAllEmployee().ToList();
+            return emps;
+        }
+        public string Create(EmployeeModel objCustomer)
+        {
+            employeeDataAccess.AddEmployee(objCustomer);
+            return "Added Successfully";
+        }
+        public EmployeeModel GetEmployeeData(int id)
+        {
+            EmployeeModel employee = employeeDataAccess.GetEmployeeData(id);
+            return employee;
+        }
+        public string DeleteEmployee(EmployeeModel employee)
+        {
+            employeeDataAccess.DeleteEmployee(employee.EmployeeId);
+            return "Delete Successfully";
+        }
+        public string UpdateEmployee(EmployeeModel objEmp)
+        {
+            employeeDataAccess.UpdateEmployee(objEmp);
+            return "Update Successful";
+        }
+    }
+}
---
+++ EmployeeServices/Services/IEmployeeService.cs
@@ -0,0 +1,14 @@
+﻿using EmpModel;
+using System.Collections.Generic;
+
+namespace EmployeeServices
+{
+    public interface IEmployeeService
+    {
+        string Create(EmployeeModel objCustomer);
+        string DeleteEmployee(EmployeeModel employee);
+        List<EmployeeModel> GetEmploess();
+        EmployeeModel GetEmployeeData(int id);
+        string UpdateEmployee(EmployeeModel objEmp);
+    }
+}
